f8a62e27fb2f887ace0aed4a09b42b400b442491
Edgar Toll <edjopato@gmail.com>
2019-04-18 12:57:50 +0200

    refactor: require LTS version of NodeJS and use newer code style

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var colorsys = require('colorsys');
-var Service, Characteristic;
+const colorsys = require('colorsys');
 
-module.exports = function(homebridge) {
+let Service;
+let Characteristic;
+
+module.exports = homebridge => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-fake-rgb", "Fake-RGB", RgbAccessory);
+  homebridge.registerAccessory('homebridge-fake-rgb', 'Fake-RGB', RgbAccessory);
 };
 
 function RgbAccessory(log, config) {
@@ -16,11 +18,11 @@ function RgbAccessory(log, config) {
   this.saturation = 0;
   this.hue = 0;
 
-  this.log("Initialized '" + this.name + "'");
+  this.log(`Initialized '${this.name}'`);
 }
 
-RgbAccessory.prototype.setColor = function() {
-  var color = colorsys.hsv_to_rgb({
+RgbAccessory.prototype.setColor = () => {
+  const color = colorsys.hsv_to_rgb({
     h: this.hue,
     s: this.saturation,
     v: this.brightness
@@ -32,57 +34,57 @@ RgbAccessory.prototype.setColor = function() {
     color.b = 0;
   }
 
-  this.log("set color to", color.r, color.g, color.b);
+  this.log('set color to', color.r, color.g, color.b);
 };
 
-RgbAccessory.prototype.getServices = function() {
-  var lightbulbService = new Service.Lightbulb(this.name);
-  var bulb = this;
+RgbAccessory.prototype.getServices = () => {
+  const lightbulbService = new Service.Lightbulb(this.name);
+  const bulb = this;
 
   lightbulbService
     .getCharacteristic(Characteristic.On)
-    .on('get', function(callback) {
+    .on('get', callback => {
       callback(null, bulb.power);
     })
-    .on('set', function(value, callback) {
+    .on('set', (value, callback) => {
       bulb.power = value;
-      bulb.log("power to " + value);
+      bulb.log(`power to ${value}`);
       bulb.setColor();
       callback();
     });
 
   lightbulbService
     .addCharacteristic(Characteristic.Brightness)
-    .on('get', function(callback) {
+    .on('get', callback => {
       callback(null, bulb.brightness);
     })
-    .on('set', function(value, callback) {
+    .on('set', (value, callback) => {
       bulb.brightness = value;
-      bulb.log("brightness to " + value);
+      bulb.log(`brightness to ${value}`);
       bulb.setColor();
       callback();
     });
 
   lightbulbService
     .addCharacteristic(Characteristic.Hue)
-    .on('get', function(callback) {
+    .on('get', callback => {
       callback(null, bulb.hue);
     })
-    .on('set', function(value, callback) {
+    .on('set', (value, callback) => {
       bulb.hue = value;
-      bulb.log("hue to " + value);
+      bulb.log(`hue to ${value}`);
       bulb.setColor();
       callback();
     });
 
   lightbulbService
     .addCharacteristic(Characteristic.Saturation)
-    .on('get', function(callback) {
+    .on('get', callback => {
       callback(null, bulb.saturation);
     })
-    .on('set', function(value, callback) {
+    .on('set', (value, callback) => {
       bulb.saturation = value;
-      bulb.log("saturation to " + value);
+      bulb.log(`saturation to ${value}`);
       bulb.setColor();
       callback();
     });
